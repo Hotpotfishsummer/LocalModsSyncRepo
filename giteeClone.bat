5194a041107deda92526b0994cfb450d8d3692b1
guanguan <x@gmail.com>
2024-09-30 18:35:32 +0800

    update bat

--- giteeClone.bat
+++ giteeClone.bat
@@ -1,3 +1,6 @@
+rem rd /s /q ".\mods"
+rem rd /s /q ".\shaderpacks"
+rem rd /s /q ".\resourcepacks"
 git clone https://gitee.com/guan_sunshine/resources.git
 xcopy ".\resources\*" ".\" /e /h
 rd /s /q ".\resources"
